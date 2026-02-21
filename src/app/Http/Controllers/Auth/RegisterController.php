9556c8454f324290ab22e651c50b55828b5b5377
root <root@TUYEN.>
2026-02-21 09:25:59 +0900

    first commit

---
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+// ▼追加
+use App\Http\Requests\UserCreateRequest;
+use App\Providers\RouteServiceProvider;
+
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+// ------ ここから追加 ------
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+// ------ ここまで追加 ------
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller {
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+    * Where to redirect users after registration.
+    *
+    * @var string
+    */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+    * Create a new controller instance.
+    *
+    * @return void
+    */
+
+    public function __construct() {
+        $this->middleware( 'guest' );
+    }
+
+    // /**
+    //  * Get a validator for an incoming registration request.
+    //  *
+    //  * @param  array  $data
+    //  * @return \Illuminate\Contracts\Validation\Validator
+    //  */
+    // protected function validator( array $data )
+    // {
+    //     return Validator::make( $data, [
+    //         'name' => [ 'required', 'string', 'max:255' ],
+    //         'email' => [ 'required', 'string', 'email', 'max:255', 'unique:users' ],
+    //         'password' => [ 'required', 'string', 'min:8', 'confirmed' ],
+    // ] );
+    // }
+
+    // /**
+    //  * Create a new user instance after a valid registration.
+    //  *
+    //  * @param  array  $data
+    //  * @return \App\Models\User
+    //  */
+    // protected function create( array $data )
+    // {
+    //     return User::create( [
+    //         'name' => $data[ 'name' ],
+    //         'email' => $data[ 'email' ],
+    //         'password' => Hash::make( $data[ 'password' ] ),
+    // ] );
+    // }
+    /**
+    * ユーザー登録後、投稿画面に遷移する
+    * @param Request $request
+    * @return RedirectResponse
+    */
+
+    public function register( UserCreateRequest $request ) {
+        $user = User::create( [
+            'name' =>  $request->name,
+            'email' => $request->email,
+            'password' => Hash::make( $request->password ),
+        ] );
+        logger()->debug($user);
+
+        $this->guard()->login( $user );
+
+        return redirect()->route( 'memo.index' );
+    }
+}
